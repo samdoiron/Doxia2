4a57265920617e558efccc38ad3bff1b439f4b60
Sabrina Gannon <sabrinagannon@icloud.com>
2020-10-18 14:07:57 -0400

    Initial implementation of Core::Handle

--- lib/core/handle.rb
+++ lib/core/handle.rb
@@ -1,5 +1,15 @@
 module Core
   class Handle
+    extend Forwardable
 
+    def initialize(handle)
+      @handle = handle
+    end
+
+    def_delegators :handle, :fetch_pages_newest_to_oldest
+
+    private
+
+    attr_accessor :handle
   end
 end
--- test/unit/core/handle_test.rb
+++ test/unit/core/handle_test.rb
@@ -7,11 +7,11 @@ module Core
       repo.transaction do |t|
         t.create_page(handle: 'my-older-page', title: 'My Older Page')
         t.create_page(handle: 'my-newer-page', title: 'My Newer Page')
+        handle = Handle.new(t)
+        pages = handle.fetch_pages_newest_to_oldest
+        assert_equal 'my-newer-page', pages.first.handle
+        assert_equal 'my-older-page', pages.second.handle
       end
-      handle = Handle.new(repo)
-      pages = handle.fetch_pages_newest_to_oldest
-      assert_equal 'my-newer-page', pages.first.handle
-      assert_equal 'my-older-page', pages.second.handle
     end
   end
 end
